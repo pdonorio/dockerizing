a84aaf9f1276f0e3abf71e7c79c3cd2e95a002e0
paulie <tempo.pause@gmail.com>
2015-11-03 00:31:30 +0100

    A working example of nginx server working on the shoulder of uwsgi python interpreter

---
+++ composer/flask/hello.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+""" Hello world for uwsgi """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
