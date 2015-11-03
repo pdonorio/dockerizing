6a3e183e81c884fee8cc93bf726a17c3c3cad7f8
paulie <tempo.pause@gmail.com>
2015-11-03 12:15:56 +0100

    Updated image to log everything as expected in /var/log. Ready to create automatic docker image repo.

--- python/nginx_uwsgi/entrypoint.sh
+++ python/nginx_uwsgi/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 /etc/init.d/nginx start
-uwsgi --ini /etc/uwsgi/vassals/uwsgi.ini # --py-autoreload 1 --py-tracebacker
+. /opt/venv/bin/activate
+uwsgi --ini /etc/uwsgi/vassals/uwsgi.ini
